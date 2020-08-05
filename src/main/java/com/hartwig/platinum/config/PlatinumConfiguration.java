9a70a67881c9cbf582cf2356f207543459fa6556
Ned Leitch <ned.leitch@gmail.com>
2020-08-05 16:52:03 -0400

    DEV-1436 Switch to JsonNode for sample JSON
    We don't actually want to parse this JSON, just pass it through
    for the later steps.

--- src/main/java/com/hartwig/platinum/config/PlatinumConfiguration.java
+++ src/main/java/com/hartwig/platinum/config/PlatinumConfiguration.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
@@ -20,7 +21,7 @@ public interface PlatinumConfiguration {
 
     OutputConfiguration outputConfiguration();
 
-    Map<String, Map<String, ?>> samples();
+    Map<String, JsonNode> samples();
 
     static PlatinumConfiguration from(File inputFile) {
         ObjectMapper objectMapper = new ObjectMapper();
