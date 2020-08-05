2716cfcb907945035c09063c4477952bd784d593
Ned Leitch <ned.leitch@gmail.com>
2020-08-05 16:41:15 -0400

    DEV-1436 Tidy up JSON parsing

--- src/main/java/com/hartwig/platinum/Platinum.java
+++ src/main/java/com/hartwig/platinum/Platinum.java
@@ -2,9 +2,6 @@ package com.hartwig.platinum;
 
 import java.io.File;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.google.cloud.storage.Storage;
 import com.hartwig.platinum.config.PlatinumConfiguration;
 import com.hartwig.platinum.kubernetes.KubernetesCluster;
@@ -23,16 +20,9 @@ public class Platinum {
     }
 
     public void run() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new Jdk8Module());
-        try {
-            PlatinumConfiguration configuration = PlatinumConfiguration.builder()
-                    .from(objectMapper.readValue(new File(input), new TypeReference<PlatinumConfiguration>() {})).runName(runName).build();
-            KubernetesCluster cluster = KubernetesCluster.findOrCreate(runName,
-                    OutputBucket.from(storage).findOrCreate(runName, configuration.outputConfiguration()));
-            cluster.submit(configuration);
-        } catch (Exception e) {
-            throw new RuntimeException("Unexpected exception", e);
-        }
+        PlatinumConfiguration configuration = PlatinumConfiguration.from(new File(input));
+        KubernetesCluster cluster = KubernetesCluster.findOrCreate(runName,
+                OutputBucket.from(storage).findOrCreate(runName, configuration.outputConfiguration()));
+        cluster.submit(configuration);
     }
 }
--- src/main/java/com/hartwig/platinum/config/PlatinumConfiguration.java
+++ src/main/java/com/hartwig/platinum/config/PlatinumConfiguration.java
@@ -1,9 +1,13 @@
 package com.hartwig.platinum.config;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
-import java.util.Optional;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
 import org.immutables.value.Value.Immutable;
 import org.immutables.value.Value.Style;
@@ -14,14 +18,18 @@ import org.immutables.value.Value.Style;
 public interface PlatinumConfiguration {
     Map<String, String> pipelineArguments();
 
-    Optional<String> runName();
-
     OutputConfiguration outputConfiguration();
 
     Map<String, Map<String, ?>> samples();
 
-    static ImmutablePlatinumConfiguration.Builder builder() {
-        return ImmutablePlatinumConfiguration.builder();
+    static PlatinumConfiguration from(File inputFile) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new Jdk8Module());
+        try {
+            return objectMapper.readValue(inputFile, new TypeReference<PlatinumConfiguration>() {});
+        } catch (IOException ioe) {
+            throw new RuntimeException("Could not parse input", ioe);
+        }
     }
 }
 
