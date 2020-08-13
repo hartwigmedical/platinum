11d7764fd160272f745ceee43ae16fe988277d54
Paul Wolfe <P.Wolfe@hartwigmedicalfoundation.nl>
2020-08-13 17:55:52 -0400

    DEV-1444 Refactor to decompose things into smaller units

--- src/main/java/com/hartwig/platinum/kubernetes/PipelineConfigMapVolume.java
+++ src/main/java/com/hartwig/platinum/kubernetes/PipelineConfigMapVolume.java
@@ -6,19 +6,20 @@ import java.util.Map.Entry;
 
 import com.hartwig.platinum.config.PlatinumConfiguration;
 
+import io.fabric8.kubernetes.api.model.Volume;
+import io.fabric8.kubernetes.api.model.VolumeBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 
-public class PipelineConfigMap {
+public class PipelineConfigMapVolume {
     private final PlatinumConfiguration configuration;
     private final KubernetesClient kubernetesClient;
 
-    public PipelineConfigMap(PlatinumConfiguration configuration, KubernetesClient kubernetesClient) {
+    public PipelineConfigMapVolume(PlatinumConfiguration configuration, KubernetesClient kubernetesClient) {
         this.configuration = configuration;
         this.kubernetesClient = kubernetesClient;
     }
 
-    public String create() {
-        String name = "samples";
+    public Volume create(final String name) {
         kubernetesClient.configMaps()
                 .createNew()
                 .addToData(configuration.samples().entrySet().stream().collect(toMap(Entry::getKey, e -> e.getValue().toString())))
@@ -27,6 +28,10 @@ public class PipelineConfigMap {
                 .withNamespace(KubernetesCluster.NAMESPACE)
                 .endMetadata()
                 .done();
-        return name;
+        return new VolumeBuilder().withName(name)
+                .editOrNewConfigMap()
+                .withName(name)
+                .endConfigMap()
+                .build();
     }
 }
---
+++ src/main/java/com/hartwig/platinum/kubernetes/PipelineServiceAccountSecretVolume.java
@@ -0,0 +1,31 @@
+package com.hartwig.platinum.kubernetes;
+
+import com.hartwig.platinum.iam.JsonKey;
+
+import org.immutables.value.internal.$processor$.meta.$FactoryMirror;
+
+import io.fabric8.kubernetes.api.model.Volume;
+import io.fabric8.kubernetes.api.model.VolumeBuilder;
+import io.fabric8.kubernetes.client.KubernetesClient;
+
+public class PipelineServiceAccountSecretVolume {
+    private final JsonKey jsonKey;
+    private final KubernetesClient kubernetesClient;
+
+    public PipelineServiceAccountSecretVolume(final JsonKey jsonKey, final KubernetesClient kubernetesClient) {
+        this.jsonKey = jsonKey;
+        this.kubernetesClient = kubernetesClient;
+    }
+
+    public Volume create(final String name) {
+        kubernetesClient.secrets()
+                .createNew()
+                .withNewMetadata()
+                .withName(name)
+                .withNamespace(KubernetesCluster.NAMESPACE)
+                .endMetadata()
+                .addToData(name, jsonKey.jsonBase64())
+                .done();
+        return new VolumeBuilder().withName(name).editOrNewConfigMap().withName(name).endConfigMap().build();
+    }
+}
