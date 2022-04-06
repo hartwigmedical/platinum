c86c805778b2a07b78ee9f3bc4c0b35a0c6eb22f
Ned Leitch <ned.leitch@gmail.com>
2022-04-06 13:22:14 -0400

    DEV-2564 Add temp debugging to launcher

--- bin/platinum.sh
+++ bin/platinum.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Running as: $(whoami)"
+echo "Gcloud config:"
+gcloud config --list
+echo "Alright running Platinum"
 /usr/bin/java ${JAVA_OPTS} -jar /usr/share/platinum/bootstrap.jar "$@"
 status=$?
 if [ ${status} -ne 0 ]; then
