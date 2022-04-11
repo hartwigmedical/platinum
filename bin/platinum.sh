c8c897c404e64e5c30685347ef289c60ba18964c
Ned Leitch <ned.leitch@gmail.com>
2022-04-11 17:26:33 -0400

    DEV-2564 Remove debugging

--- bin/platinum.sh
+++ bin/platinum.sh
@@ -1,35 +1,8 @@
 #!/bin/bash
 
-echo "Running as: $(whoami)"
-echo "Environment:"
-env
-echo "Specifically ADC: $GOOGLE_APPLICATION_CREDENTIALS"
-echo "Access token: $(gcloud auth application-default print-access-token)"
-echo "Key contents: $(cat $GOOGLE_APPLICATION_CREDENTIALS)"
 gcloud auth activate-service-account --key-file=/google-key.json
-echo "Gcloud config:"
-gcloud config list
-echo "Config directory:"
-ls -l /root/.config/gcloud
-echo "Getting clusters:"
 gcloud container clusters get-credentials patient-cluster-verification-1 --region europe-west4 --project hmf-crunch
-echo "Containers:"
-gcloud container clusters list
-echo "Kubectl config:"
-kubectl config view
-echo "Control server:"
-echo "Ping:"
-ping -c1 192.168.23.50
-echo "Curl to https:"
-curl https://192.168.23.50
-echo "Local IP:"
-ifconfig -a
-ip addr
-
 kubectl create clusterrolebinding default-role --clusterrole=edit --serviceaccount=default:default --namespace=default
-
-echo "Alright running Platinum"
-export GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS
 /usr/bin/java ${JAVA_OPTS} -jar /usr/share/platinum/bootstrap.jar "$@"
 status=$?
 if [ ${status} -ne 0 ]; then
