132cdddfe8420c830f4c265e50e55096945d9397
Ned Leitch <ned.leitch@gmail.com>
2023-05-29 18:05:11 -0400

    DEV-3228 Leverage API canonical set resolution
    This was just added to the API and greatly simplifies the code here.

--- src/main/java/com/hartwig/platinum/ApiRerun.java
+++ src/main/java/com/hartwig/platinum/ApiRerun.java
@@ -2,9 +2,7 @@ package com.hartwig.platinum;
 
 import static java.lang.String.format;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 import com.hartwig.api.RunApi;
 import com.hartwig.api.SampleApi;
@@ -39,36 +37,12 @@ public class ApiRerun {
     }
 
     public long create(final String tumorSampleName) {
-        List<SampleSet> sets = setApi.list(null,
-                sampleApi.list(null, null, null, null, SampleType.TUMOR, tumorSampleName, null)
-                        .stream()
-                        .findFirst()
-                        .orElseThrow(() -> new IllegalArgumentException(format("Cannot find tumor sample [%s]", tumorSampleName)))
-                        .getId(),
-                true);
-        if (sets.isEmpty()) {
-            throw new IllegalArgumentException(format("No sets with consent for the database could be found for [%s]", tumorSampleName));
-        }
-        return getOrCreateRun(tumorSampleName, sets.size() == 1 ? sets.get(0) : resolveActiveSet(sets, tumorSampleName));
-    }
-
-    private SampleSet resolveActiveSet(List<SampleSet> sets, String tumorSampleName) {
-        List<SampleSet> setsWithNonInvalidatedRuns = new ArrayList<>();
-        for (SampleSet set : sets) {
-            List<Run> runs = runApi.list(null, null, set.getId(), null, null, null, null, null);
-            if (runs.stream().anyMatch(r -> r.getStatus() != Status.INVALIDATED)) {
-                setsWithNonInvalidatedRuns.add(set);
-            }
-        }
-        if (setsWithNonInvalidatedRuns.isEmpty()) {
-            throw new IllegalStateException(format("Multiple sets found for tumor sample [%s] but none have non-invalidated runs attached",
-                    tumorSampleName));
-        } else if (setsWithNonInvalidatedRuns.size() > 1) {
-            throw new IllegalStateException(format("Multiple sets with non-invalidated runs found for tumor sample [%s]: %s",
-                    tumorSampleName,
-                    setsWithNonInvalidatedRuns.stream().map(s -> s.getId().toString()).collect(Collectors.joining(","))));
-        }
-        return setsWithNonInvalidatedRuns.get(0);
+        SampleSet set = Optional.ofNullable(setApi.canonical(sampleApi.list(null, null, null, null, SampleType.TUMOR, tumorSampleName, null)
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(format("Cannot find tumor sample [%s]", tumorSampleName)))
+                .getId())).orElseThrow(() -> new IllegalArgumentException(format("No sets with consent for the database could be found for [%s]", tumorSampleName)));
+        return getOrCreateRun(tumorSampleName, set);
     }
 
     /**
