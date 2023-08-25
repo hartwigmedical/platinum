e95dd18a42129dc8ff2efbf40148a9eee2e1e56f
Ned Leitch <ned.leitch@gmail.com>
2023-08-25 17:26:53 -0400

    DEV-3552 Improve usability for large batches
    The disused `PlatinumResult` class has been repurposed to help provide better
    information on completion. Logging has been added to help track down failures
    when they happen. More operations have been covered by auth-and-retry in the
    `KubernetesClientProxy` class.
    
    Finally summary information has been augmented to indicate how many failures
    as well as successful submissions were completed.

--- src/main/java/com/hartwig/platinum/kubernetes/KubernetesClientProxy.java
+++ src/main/java/com/hartwig/platinum/kubernetes/KubernetesClientProxy.java
@@ -1,12 +1,6 @@
 package com.hartwig.platinum.kubernetes;
 
-import static java.util.List.of;
-
 import com.hartwig.platinum.config.GcpConfiguration;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.fabric8.kubernetes.api.model.ConfigMapList;
 import io.fabric8.kubernetes.api.model.Secret;
@@ -15,10 +9,17 @@ import io.fabric8.kubernetes.api.model.batch.Job;
 import io.fabric8.kubernetes.api.model.batch.JobList;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
 import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.NonNamespaceOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
 import io.fabric8.kubernetes.client.dsl.ScalableResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Provider;
+
+import static java.util.List.of;
 
 public class KubernetesClientProxy {
     private static final Logger LOGGER = LoggerFactory.getLogger(KubernetesClientProxy.class);
@@ -36,15 +37,29 @@ public class KubernetesClientProxy {
     }
 
     public MixedOperation<ConfigMap, ConfigMapList, Resource<ConfigMap>> configMaps() {
-        return kubernetesClient.configMaps();
+        return executeWithRetries(() -> kubernetesClient.configMaps());
     }
 
     public MixedOperation<Secret, SecretList, Resource<Secret>> secrets() {
-        return kubernetesClient.secrets();
+        return executeWithRetries(() -> kubernetesClient.secrets());
     }
 
     public NonNamespaceOperation<Job, JobList, ScalableResource<Job>> jobs() {
-        return kubernetesClient.batch().jobs().inNamespace(KubernetesCluster.NAMESPACE);
+        try {
+            return kubernetesClient.batch().jobs().inNamespace(KubernetesCluster.NAMESPACE);
+        } catch (KubernetesClientException e) {
+            authorise();
+            return jobs();
+        }
+    }
+
+    private MixedOperation executeWithRetries(Provider<MixedOperation<?, ?, ?>> operationProvider) {
+        try {
+            return operationProvider.get();
+        } catch (KubernetesClientException e) {
+            authorise();
+            return executeWithRetries(operationProvider);
+        }
     }
 
     public void authorise() {
